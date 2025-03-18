d018969827c2bd303bbf999e2ff22fdc14f6f536
ira <irishaniro@yandex.ru>
2025-03-18 18:34:26 +0300

    Margorina

--- build/CMakeFiles/sfml_test.dir/DependInfo.cmake
+++ build/CMakeFiles/sfml_test.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/home/daria/Project-2025/main.cpp" "CMakeFiles/sfml_test.dir/main.cpp.o" "gcc" "CMakeFiles/sfml_test.dir/main.cpp.o.d"
+  "/home/ira/Project-2025/main.cpp" "CMakeFiles/sfml_test.dir/main.cpp.o" "gcc" "CMakeFiles/sfml_test.dir/main.cpp.o.d"
   )
 
 # Targets to which this target links which contain Fortran sources.
--- main.cpp
+++ main.cpp
@@ -5,6 +5,23 @@ int main() {
     setlocale(LC_ALL, "RUS");
     sf::RenderWindow window(sf::VideoMode(1920, 1080), "Japanese Crossword");
 
+    sf::Texture backgroundTexture;
+    if (!backgroundTexture.loadFromFile("fon2.jpg")) {
+        return -1; // Ошибка загрузки текстуры
+    }
+
+    // Создание спрайта для фона
+    sf::Sprite backgroundSprite;
+    backgroundSprite.setTexture(backgroundTexture);
+
+    // Масштабирование спрайта под размер окна (опционально)
+    sf::Vector2u windowSize = window.getSize();
+    sf::Vector2u textureSize = backgroundTexture.getSize();
+    backgroundSprite.setScale(
+        static_cast<float>(windowSize.x) / textureSize.x,
+        static_cast<float>(windowSize.y) / textureSize.y
+    );
+
     sf::RectangleShape button(sf::Vector2f(200, 50));
     button.setPosition(1400, 900);
     button.setFillColor(sf::Color::Green);
@@ -41,7 +58,7 @@ int main() {
         }
 
         window.clear();
-
+        window.draw(backgroundSprite);
         window.draw(square);
         window.draw(button);
         window.draw(buttonText);
